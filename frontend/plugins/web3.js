193ab42a7dd7628a8dd18ad80c0c61c21304da01
John Azar (PC) <johnazar@users.noreply.github.com>
2022-04-24 11:32:20 +0300

    update

--- frontend/plugins/web3.js
+++ frontend/plugins/web3.js
@@ -1,12 +1,15 @@
 import Web3 from 'web3'
 
-export default ({store}, inject) => {
+export default ({ store }, inject) => {
   let web3 = null
   if (!process.client) return // don't run on serverside
-  web3 = window.ethereum ? new Web3(window.ethereum) : null
+  web3 =
+    window.ethereum && window.ethereum.isMetaMask
+      ? new Web3(window.ethereum)
+      : null
   inject('web3', web3)
   // Inject the $web3 instance into the context object in Nuxt
-  if (!web3) return 
+  if (!web3) return
   const contract = new web3.eth.Contract(
     store.state.contract.HelloDApp_abi, // コントラクトのコンパイル後の設定ファイル
     store.state.contract.HelloDApp_contract // ネットワークIDごとに保存されているコントラクトのアドレスを読み込む

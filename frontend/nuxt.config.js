9a8cfe9a3ce09ddb18a88f508ea7600041a48962
John Azar (PC) <johnazar@users.noreply.github.com>
2022-04-27 01:17:53 +0300

    docker

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -78,4 +78,11 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
+  watchers: {
+    webpack: {
+      ignored: /node_modules/,
+      aggregateTimeout: 300,
+      poll: 1000
+    }
+  }
 }
